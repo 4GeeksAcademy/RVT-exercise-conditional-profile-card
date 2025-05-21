066f2e08e7c013e12d03b556f12a660ca74b3c52
Romina Verónica Torres <110548507+RomiTorres@users.noreply.github.com>
2025-05-21 19:28:05 +0000

    Empezado sin funcionar

--- src/js/app.js
+++ src/js/app.js
@@ -29,11 +29,17 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  let nombre = "";
+  if (variables.name) {
+    nombre = variables.name;
+  } else {
+    nombre = " ";
+  }
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
+          <h1>${variables.name} ${variables.lastname} </h1>
           <h2>Web Developer</h2>
           <h3>Miami, USA</h3>
           <ul class="position-right">
